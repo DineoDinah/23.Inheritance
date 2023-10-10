1f5c863802abadac3828c3fbf96ef5d0e8bcc9a9
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-10 09:49:41 +0200

    Add project files.

---
+++ 23.Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23.Inheritance
+{
+    
+       internal class Chef
+       { 
+        public void MakeChicken()
+        {
+            Console.WriteLine("The chef makes Chicken");
+
+        }
+        public void MakeSalad()
+        {
+            Console.WriteLine("The chef makes a salad");
+        }
+        public virtual void MakeSpecialDish() //virtual allows subclasses to override
+        {
+            Console.WriteLine("The Chef makes special dish");
+
+        }
+        internal class ItalianChef : Chef //this makes Italian Chef inheritall there is in Chef, can now call methods in chef!                                           
+        {
+            public void MakePasta()
+            {
+                Console.WriteLine("Makes Pasta");
+            }
+            public override void MakeSpecialDish() //use keyword override
+            {
+                Console.WriteLine("The chef makes Italianspecials");
+            }
+
+
+           
+       }
+    }
+}
